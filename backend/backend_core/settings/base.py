4622c3a1891197629ce2f97b1814049ec7ec85e7
Анна <@ANNAB-25>
2026-06-11 18:28:42 +0300

    Fix

--- backend/backend_core/settings/base.py
+++ backend/backend_core/settings/base.py
@@ -122,9 +122,14 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://skinmatcby-production.up.railway.app",
 ]
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://skinmatcby-production.up.railway.app',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
